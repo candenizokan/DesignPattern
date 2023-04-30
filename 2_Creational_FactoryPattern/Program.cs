e77ff3b410c15fd813086887e50eb6c8d7fc0ea7
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 12:45:31 +0300

    2_Creational_FactoryPattern

---
+++ 2_Creational_FactoryPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Creational_FactoryPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+             Factory Design Pattern
+
+            Creational grubuna ait, aynı arayüzü kullanan nesnelerin üretiminden sorumlu tasarım desenidir. Kullanım oranı fazladır.
+
+            * Bu yaratıcı desen, her hangi bir uygulamanın hedeflediği yerden kullanılabilir, bakımı yapılabilir, test edilebilir bir yapı oluşturmmamızı sağlar. Bunun günlük hayattaki en somut örnekleri fabrikalardır.
+            * 
+            * * Bir fabrikanın amacı bir ürün üretmektir. Örneğin telefon üreten bir fabrika olduğunu düşünelim. Tek amacı telefon üretmektir ve müşteriler yeni tüketiciler içerde işlerin nasıl yürüdüğüyle ilgilenmezler.
+            * İlgilendikleri telefonu kullanabilmektir.
+            * 
+            * Kuvvetli bir desendir. genişletilebilir ve modifikasyon sağlayabilir ancak yapılar büyüdüğünde karmaşıklaşmaya başlar dikkatli olmak gerekir.
+            * 
+            * Kalıtımsal ilişkileri olan nesnelerin üretimi için esas olan 1 metod vardır ve bu metod uygulamanın ihtiyacı olan nesnelerin üretiminde sorumludur. yani fabrika görevi görür.
+            * 
+            * Factory ve Abstract Factory farklı desenlerdir.
+            * 
+            * 
+            * Esas metodu barındıran sınıf
+            * Uygulamanın ihtiyacı olan nesnelerin arayüzü
+            * nesneler.
+             */
+        }
+    }
+}
