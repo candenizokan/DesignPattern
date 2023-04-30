70684f9d22223fb9c08e75eba9f18cfdcedcf028
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 11:51:52 +0300

    Singleton design pattern açıklaması

--- 1_Creational_SingletonPattern/Program.cs
+++ 1_Creational_SingletonPattern/Program.cs
@@ -10,6 +10,16 @@ namespace _1_Creational_SingletonPattern
     {
         static void Main(string[] args)
         {
+            /*
+             Singleton Design Pattern => yapı açısından basittir. bu özel bir örnekleme ve bu örnekleme başvurmanın tek yolu sağlayan static bir yönteme sahip tek bir sınıftır.
+
+            Private işaretlenmiş bir constructor olmalıdır.
+            Çünkü nesnenin dışarıdan üretilmemesi içeride tek bir sefer oluşturulup sürekli aynı nesnenin kullanılması istenir.
+            Diğer bir deyişle nesnein kullanım süresi boyunca bir sefer oluşturulup çokça kullanılması mantığına dayanır.
+
+            Singleton ile üretilen nesnenin kendine has bir yaşam döngüsü vardır bu yüzden her önümüze gelen nesneyi bu şekilde oluşturamayız. Singleton nesneler statik sınıflar gibi RAM üzerinde yaşam alanlarında biricikklerdir. Dikkatli kullanmak gerekir
+             
+             */
         }
     }
 }
