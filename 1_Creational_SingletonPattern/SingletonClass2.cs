1a6f54d358464e40dacb92649ab5cda94d3a11d9
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 12:25:39 +0300

    s2

---
+++ 1_Creational_SingletonPattern/SingletonClass2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Creational_SingletonPattern
+{
+    public class SingletonClass2
+    {
+        //private ctor
+        //private static field
+        // sınıf içinde oluşan nesneyi paylaşarak public property / metod
+        private SingletonClass2()
+        {
+
+        }
+
+        private static SingletonClass2 _nesne;
+
+        public static SingletonClass2 Yetenek()
+        {
+            if( _nesne == null)
+            {
+                _nesne = new SingletonClass2();
+
+            }
+            return _nesne;
+            
+        }
+
+        public static SingletonClass2 Ozellik
+        {
+            get
+            {
+                if (_nesne == null) _nesne = new SingletonClass2();
+                return (_nesne);
+               
+            }
+        }
+
+        public int Hesapla(int s1, int s2) => s1 + s2;
+    }
+}
