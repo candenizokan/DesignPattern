162e327d7eb36372abded11196f3792bb1339ccc
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 12:55:39 +0300

    windows

---
+++ 2_Creational_FactoryPattern/WindowsApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Creational_FactoryPattern
+{
+    public class WindowsApplication : MobileApplication
+    {
+        public override void Platform()
+        {
+            //kod blok
+        }
+    }
+}
