b805a9c288154412a54573631bd9ef24a77226bd
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 12:54:43 +0300

    iphone

---
+++ 2_Creational_FactoryPattern/IPhoneApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Creational_FactoryPattern
+{
+    //fabrikadan oluşturulacak, arayüzü kullanan sınıf
+    public class IPhoneApplication : MobileApplication
+    {
+        public override void Platform()
+        {
+            //kod blok
+        }
+    }
+}
