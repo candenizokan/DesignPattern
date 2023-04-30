1a9b23f95148669cc8e9e9f765ec4044faa4c410
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 12:09:46 +0300

    private ctoru dışara çıkarma yollu static field veya method

--- 1_Creational_SingletonPattern/SingletonClass.cs
+++ 1_Creational_SingletonPattern/SingletonClass.cs
@@ -23,5 +23,17 @@ namespace _1_Creational_SingletonPattern
 
         // ctor çalıştırılıp nesney, aldık lakin hala dışarıdan erişilemez bir nesne!
         private static SingletonClass _tekNesne=new SingletonClass();//ne stackte ne heapte static ramde ayrı bir alanda
+
+        //içeridiki fieldı dışarıda kullanmak için get yapmam lazım set olmayacak
+        public static SingletonClass Nesnem { get { return _tekNesne; } }// içerideki fieldın taşıdığı nesneyi dışarı çıkarmaya yarıyor. static işaretli
+
+        //bunu metod ilede yapabilirim. fielddaki nesneyi dışarısı ile paylaşan public işaretli bir metod yada property oluşturduk ! static işaretli!
+        public static SingletonClass Metodum()
+        {
+            return _tekNesne;
+        }
+
+        public static int Topla(int sayi1, int sayi2) => sayi1 + sayi2;
+
     }
 }
