7bd0312a6544695ad7d461e509ea7dfe73cc4be3
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 13:02:53 +0300

    create enum

---
+++ 2_Creational_FactoryPattern/AllMobile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Creational_FactoryPattern
+{
+    internal enum AllMobile
+    {
+        Android,
+        IPhone,
+        Windows
+    }
+}
