08de6dc4184b8b8450c1573ac2a0268b053dac28
Okan Candeniz <candenizokan@gmail.com>
2023-05-07 11:38:38 +0300

    Adapter Pattern

--- 4_Structural_AdapterPattern/Program.cs
+++ 4_Structural_AdapterPattern/Program.cs
@@ -10,6 +10,13 @@ namespace _4_Structural_AdapterPattern
     {
         static void Main(string[] args)
         {
+            /*
+             
+             Bağdaştırıcı(adapter) desenin mantığı uyumsuz yapılar/arabirimleri uyumlu hale getirmektir. Bir veya birden fazla servis sağlayıcısının kullanılması gereken olumsuz durumlarda uyumsuz yapılar arasına bir arayüz planlanarak çözüme gidilmeye çalışılır.
+            Örneğin ; TR de sorunsuz çalışan bir pc'miz var. Yurtdışına çıktığımızda orada prizlerde uymlu olmadığınız için şarjımızı kullanmayıp sorun yaşanabilir. uyumsuzluk noktasında şarjımızı yada prizi değiştirmedekten ziyade ikisini birbirine adapte eden bir parça ile bu sorunu gidermeye çalışıyoruz.
+
+            Bu pattern özellikle hazır kod bloklarında oluşan frameworkler, kütüphaneler için kullanışlıdır çünkü bu yapılar ile bizim kodlarımız farklıdır lakin bazen oluşturup ilgili kütüphaneyi implemante ettiğimiz bir sınıf ile bazen bir metod çağrımı ile arayı bulmaya çalışır. mantık buradan gelir
+             */
         }
     }
 }
