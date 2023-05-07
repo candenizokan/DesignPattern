cac0287cb0b0b2bb908a369118c15ad8ccdcee17
Okan Candeniz <candenizokan@gmail.com>
2023-05-07 10:49:19 +0300

    HandleMP4

---
+++ 3_Behavioral_ChainOfResponsibilityPattern/HandleMP4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Behavioral_ChainOfResponsibilityPattern
+{
+    internal class HandleMP4 : PlayerHandler
+    {
+        public override void Play(string fileType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
