058293244f01e64a4744c6ebc0aa94bd6498a4be
Okan Candeniz <candenizokan@gmail.com>
2023-05-07 10:48:36 +0300

    _nextPlayer

--- 3_Behavioral_ChainOfResponsibilityPattern/HandlerMP3.cs
+++ 3_Behavioral_ChainOfResponsibilityPattern/HandlerMP3.cs
@@ -8,9 +8,19 @@ namespace _3_Behavioral_ChainOfResponsibilityPattern
 {
     internal class HandlerMP3 : PlayerHandler
     {
-        public override void Play(string fileType)
+        public override void Play(string fileType)//string bir dosya uzantısı alacaksın
         {
-            throw new NotImplementedException();
+            if (fileType.EndsWith(".Mp3"))//EndsWith ilgili string ifadede sonu mp3 le bitiyor mu true yada false gelecek
+            {
+                Console.WriteLine("mp3 dosyası okunuyor..");
+            }
+            else
+            {
+                if (_nextPlayer !=null)
+                {
+                    _nextPlayer.Play(fileType);//benden sonraki player _nextPlayer boş değilse benden sonraki halkaya aktar
+                }
+            }
         }
     }
 }
