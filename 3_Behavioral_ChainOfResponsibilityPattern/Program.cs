cbf8639769ec8335f6908c7c7d24e2156a32d705
Okan Candeniz <candenizokan@gmail.com>
2023-05-07 10:30:43 +0300

    description

--- 3_Behavioral_ChainOfResponsibilityPattern/Program.cs
+++ 3_Behavioral_ChainOfResponsibilityPattern/Program.cs
@@ -10,6 +10,20 @@ namespace _3_Behavioral_ChainOfResponsibilityPattern
     {
         static void Main(string[] args)
         {
+
+            /*
+             Chain of Responsibility - Sorumluluk Zinciri
+            * Sorumluluk zincirinde temelde 3 bileşen bulunur. Uygulamaya çağrıda bulunan bir gönderici(sender), alıcı(reciver) ve zincir boyunca çalışacak bir işleyicimiz (handler) bulunur. Bu desen gelen isteği handle etmek için, işlemek için zincir boyunca ilerler.
+            * 
+            * Çalışma prensibine göre önce sorumluluk zinciri kurulur. Her bir halka gelen isteği işleme kapasitesindedir. Gelen istek karşılanabiliyorsa yapılır eğer o halkada o yetki yoksa diğer zincire aktarılır.
+            * 
+            * Sorumluluk zincirinde farklı handlerlar aynı anda çalışabilir.
+            * 
+            * ÖRNEĞİN: bankaya kredi çekmeye gittiğinizde 1 milyon kredi isteğiniz var. Veznedeki çalışanın bu yetkisi yoksa isteğiniz şube müdürüne, onunda yekisi yoksa bölge müdürüne ve onunda yetkisi yoksa genel müdüre aktarılır. İsteğiniz her hangi bir aşamada karşılık bulursa işlem tamamlanır ancak hiç karşılık bulmamışda olabilir.
+            * 
+            * ASP.NET pipeline(akış şeması) bu desen mantığında çalışır.
+             
+             */
         }
     }
 }
